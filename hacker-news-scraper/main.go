5dc4a83771c2afae816dd1b2a9c4ed84b03d6df2
Fernando Burillo López <fburillo@ucm.es>
2024-06-05 10:11:20 +0200

    Refactoring to decouple services and build unit testing

--- hacker-news-scraper/main.go
+++ hacker-news-scraper/main.go
@@ -12,35 +12,39 @@ import (
 const hnApiUrl = "https://hacker-news.firebaseio.com/v0"
 const maxReturnItems = 10
 
-func RetrieveHackerNewsItems(w http.ResponseWriter, _ *http.Request) {
+func RetrieveHackerNewsItems(retriever services.Retriever) http.HandlerFunc {
+	return func(w http.ResponseWriter, _ *http.Request) {
 
-	var retriever services.Retriever
-	retriever = &services.APIConnector{Url: hnApiUrl, MaxResults: maxReturnItems, ItemsEndPoint: "topstories", ItemDataEndPoint: "item"}
-	items, err := retriever.GetItems()
-	if err != nil {
-		log.Printf("Failed to get Items: %v\n", err)
-		http.Error(w, "Error obtaining required data", http.StatusInternalServerError)
-		return
-	}
+		items, err := retriever.GetItems()
+		if err != nil {
+			log.Printf("Failed to get Items: %v\n", err)
+			http.Error(w, "Error obtaining required data", http.StatusInternalServerError)
+			return
+		}
 
-	// Setting the default content-type header to JSON.
-	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	response := make([]data.ScraperResponse, len(items))
-	for i, item := range items {
-		response[i] = data.ScraperResponse{Title: item.Title}
-	}
-	err = json.NewEncoder(w).Encode(response)
-	if err != nil {
-		log.Printf("Failed to build response: %v", err)
-		http.Error(w, "Error building service response", http.StatusInternalServerError)
-		return
+		// Setting the default content-type header to JSON.
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		response := make([]data.ScraperResponse, len(items))
+		for i, item := range items {
+			response[i] = data.ScraperResponse{Title: item.Title}
+		}
+		err = json.NewEncoder(w).Encode(response)
+		if err != nil {
+			log.Printf("Failed to build response: %v", err)
+			http.Error(w, "Error building service response", http.StatusInternalServerError)
+			return
+		}
 	}
 }
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/hacker-news-items", RetrieveHackerNewsItems).Methods("GET")
+
+	var retriever services.Retriever
+	retriever = &services.APIConnector{Url: hnApiUrl, MaxResults: maxReturnItems, ItemsEndPoint: "topstories", ItemDataEndPoint: "item"}
+	hackerRankRetrieveHandler := RetrieveHackerNewsItems(retriever)
+	r.HandleFunc("/hacker-news-items", hackerRankRetrieveHandler).Methods("GET")
 	http.Handle("/", r)
 
 	log.Println("Starting server on :8080")
--- hacker-news-scraper/services/itemRetriever.go
+++ hacker-news-scraper/services/itemRetriever.go
@@ -2,6 +2,8 @@ package services
 
 import "github.com/IntelligenzCodeLab/hacker-news-scraper/data"
 
+//go:generate mockgen -source=itemRetriever.go -destination=mock/itemRetriever.go
+
 type Retriever interface {
 	GetItems() ([]data.Item, error)
 }
